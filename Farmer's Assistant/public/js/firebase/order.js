1a64321aa32c1aed92b57cbb87598ea12aa9d807
damithde <damithdesilva2@gmail.com>
2018-11-28 11:48:06 +0530

    added cart

--- Farmer's Assistant/public/js/firebase/order.js
+++ Farmer's Assistant/public/js/firebase/order.js
@@ -43,8 +43,21 @@ function getallstocks(){
     
 }
 
-function getorders(id){
-    console.log(id);
+function addtocart(id){
+    var docRef = db.collection("stock").doc(id);
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            //get the data by using doc.data().whatevertheattribute u want eg: doc.data().perprice for price 
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+
 
 }
 
